807f4067a450d578ba803baf04372e1f4512bbd6
Anton Skovpen <anton.skovpen@gmail.com>
2018-07-08 01:11:49 -0700

    Added: Cookie

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import ToDoList from 'containers/ToDoList';
 import Authorization from 'containers/Authorization';
-import ButtonAppBar from 'containers/NavMenu';
 
 class App extends Component {
   render() {
@@ -11,18 +10,7 @@ class App extends Component {
       <Router>
         <div>
           <Route path="/" exact component={Authorization} />
-          <Route
-            path="/toDoList"
-            exact
-            render={() => {
-              return (
-                <div>
-                  <ButtonAppBar />
-                  <ToDoList />;
-                </div>
-              );
-            }}
-          />
+          <Route path="/toDoList" exact component={ToDoList} />
         </div>
       </Router>
     );
